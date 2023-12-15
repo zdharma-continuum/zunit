3f0938550b278a8f608cdc66fa4d672577dada12
Vladislav Doster <mvdoster@gmail.com>
2023-12-15 13:25:33 -0600

    feat: add vim folds and format
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- src/assertions.zsh
+++ src/assertions.zsh
@@ -1,437 +1,382 @@
+# vim: ft=zsh sw=4 ts=4 et foldmarker=[[[,]]] foldmethod=marker
+
 ################################
 # Internal assertion functions #
 ################################
 
-###
-# Assert one string is a substring of another
-###
-function _zunit_assert_is_substring_of() {
-  local value=$1 comparison=$2
-
-  [[ "$comparison" = *"$value"* ]] && return 0
-
-  echo "'$value' is not a substring of '$comparison'"
-  exit 1
-}
-
-###
-# Assert one string is not a substring of another
-###
-function _zunit_assert_is_not_substring_of() {
-  local value=$1 comparison=$2
-
-  [[ "$comparison" != *"$value"* ]] && return 0
-
-  echo "'$value' is a substring of '$comparison'"
-  exit 1
-}
-
-###
+# FUNCTION: _zunit_assert_contains [[[
 # Assert one string contains another
-###
 function _zunit_assert_contains() {
-  local value=$1 comparison=$2
+    local value=$1 comparison=$2
 
-  [[ "$value" = *"$comparison"* ]] && return 0
+    [[ "$value" = *"$comparison"* ]] && return 0
 
-  echo "'$value' does not contain '$comparison'"
-  exit 1
-}
-
-###
-# Assert one string does not contain another
-###
-function _zunit_assert_does_not_contain() {
-  local value=$1 comparison=$2
-
-  [[ "$value" != *"$comparison"* ]] && return 0
-
-  echo "'$value' contains '$comparison'"
-  exit 1
-}
-
-###
-# Assert that two integers are equal
-###
-function _zunit_assert_equals() {
-  local value=$1 comparison=$2
-
-  [[ $value -eq $comparison ]] && return 0
-
-  echo "'$value' is not equal to '$comparison'"
-  exit 1
-}
-
-###
-# Assert that two integers are not equal
-###
-function _zunit_assert_not_equal_to() {
-  local value=$1 comparison=$2
-
-  [[ $value -ne $comparison ]] && return 0
-
-  echo "'$value' is equal to '$comparison'"
-  exit 1
-}
-
-###
-# Assert that an integer is positive
-###
-function _zunit_assert_is_positive() {
-  local value=$1 comparison=$2
-
-  [[ $value -gt 0 ]] && return 0
-
-  echo "'$value' is not positive"
-  exit 1
-}
-
-###
-# Assert that an integer is negative
-###
-function _zunit_assert_is_negative() {
-  local value=$1 comparison=$2
-
-  [[ $value -lt 0 ]] && return 0
-
-  echo "'$value' is not negative"
-  exit 1
-}
-
-###
-# Assert that an integer is greater than the comparison
-###
-function _zunit_assert_is_greater_than() {
-  local value=$1 comparison=$2
-
-  [[ $value -gt $comparison ]] && return 0
-
-  echo "'$value' is not greater than '$comparison'"
-  exit 1
-}
-
-###
-# Assert that an integer is less than the comparison
-###
-function _zunit_assert_is_less_than() {
-  local value=$1 comparison=$2
-
-  [[ $value -lt $comparison ]] && return 0
-
-  echo "'$value' is not less than '$comparison'"
-  exit 1
-}
-
-###
-# Assert that two string are the same
-###
-function _zunit_assert_same_as() {
-  local value=$1 comparison=$2
-
-  [[ $value = $comparison ]] && return 0
-
-  echo "'$value' is not the same as '$comparison'"
-  exit 1
-}
-
-###
+    echo "'$value' does not contain '$comparison'"
+    exit 1
+} # ]]]
+# FUNCTION: _zunit_assert_different_to [[[
 # Assert that two string are different
-###
 function _zunit_assert_different_to() {
-  local value=$1 comparison=$2
+    local value=$1 comparison=$2
 
-  [[ $value != $comparison ]] && return 0
+    [[ $value != $comparison ]] && return 0
 
-  echo "'$value' is the same as '$comparison'"
-  exit 1
-}
+    echo "'$value' is the same as '$comparison'"
+    exit 1
+} # ]]]
+# FUNCTION: _zunit_assert_does_not_contain [[[
+# Assert one string does not contain another
+function _zunit_assert_does_not_contain() {
+    local value=$1 comparison=$2
 
-###
-# Assert that a value is empty
-###
-function _zunit_assert_is_empty() {
-  local value=$1
+    [[ "$value" != *"$comparison"* ]] && return 0
 
-  [[ -z ${value[@]} ]] && return 0
-
-  echo "'${value[@]}' is not empty"
-  exit 1
-}
-
-###
-# Assert that a value is not empty
-###
-function _zunit_assert_is_not_empty() {
-  local value=$1
-
-  [[ -n ${value[@]} ]] && return 0
-
-  echo "value is empty"
-  exit 1
-}
-
-###
-# Assert that the value matches a regex pattern
-###
-function _zunit_assert_matches() {
-  local value=$1 pattern=$2
-
-  [[ $value =~ $pattern ]] && return 0
-
-  echo "'$value' does not match /$pattern/"
-  exit 1
-}
-
-###
+    echo "'$value' contains '$comparison'"
+    exit 1
+} # ]]]
+# FUNCTION: _zunit_assert_does_not_match [[[
 # Assert that the value does not match a regex pattern
-###
 function _zunit_assert_does_not_match() {
-  local value=$1 pattern=$2
+    local value=$1 pattern=$2
 
-  [[ ! $value =~ $pattern ]] && return 0
+    [[ ! $value =~ $pattern ]] && return 0
 
-  echo "'$value' matches /$pattern/"
-  exit 1
-}
+    echo "'$value' matches /$pattern/"
+    exit 1
+} # ]]]
+# FUNCTION: _zunit_assert_equals [[[
+# Assert that two integers are equal
+function _zunit_assert_equals() {
+    local value=$1 comparison=$2
 
-###
-# Assert that a value is found in an array
-###
-function _zunit_assert_in() {
-  local i found=0 value=$1
-  local -a array
-  array=(${(@)@:2})
+    [[ $value -eq $comparison ]] && return 0
 
-  for i in ${(@f)array}; do
-    [[ $i = $value ]] && found=1
-  done
-
-
-  [[ $found -eq 1 ]] && return 0
-
-  echo "'$value' is not in (${(@f)array})"
-  exit 1
-}
-
-###
-# Assert that a value is not found in an array
-###
-function _zunit_assert_not_in() {
-  local i found=0 value=$1
-  local -a array
-  array=(${(@)@:2})
-
-  for i in ${(@f)array}; do
-    [[ $i = $value ]] && found=1
-  done
-
-  [[ $found -eq 0 ]] && return 0
-
-  echo "'$value' is in (${(@f)array})"
-  exit 1
-}
-
-###
-# Assert that a value is a key in a hash
-###
-function _zunit_assert_is_key_in() {
-  local i found=0 value=$1
-  local -A hash
-  hash=(${(@)@:2})
-
-  for k v in ${(@kv)hash}; do
-    [[ $k = $value ]] && found=1
-  done
-
-  [[ $found -eq 1 ]] && return 0
-
-  echo "'$value' is not a key in (${(@kv)hash})"
-  exit 1
-}
-
-###
-# Assert that a value is not a key in a hash
-###
-function _zunit_assert_is_not_key_in() {
-  local i found=0 value=$1
-  local -A hash
-  hash=(${(@)@:2})
-
-  for k v in ${(@kv)hash}; do
-    [[ $k = $value ]] && found=1
-  done
-
-  [[ $found -eq 0 ]] && return 0
-
-  echo "'$value' is a key in (${(@kv)hash})"
-  exit 1
-}
-
-###
-# Assert that a value is a value in a hash
-###
-function _zunit_assert_is_value_in() {
-  local i found=0 value=$1
-  local -A hash
-  hash=(${(@)@:2})
-
-  for k v in ${(@kv)hash}; do
-    [[ $v = $value ]] && found=1
-  done
-
-  [[ $found -eq 1 ]] && return 0
-
-  echo "'$value' is not a value in (${(@kv)hash})"
-  exit 1
-}
-
-###
-# Assert that a value is not a value in a hash
-###
-function _zunit_assert_is_not_value_in() {
-  local i found=0 value=$1
-  local -A hash
-  hash=(${(@)@:2})
-
-  for k v in ${(@kv)hash}; do
-    [[ $v = $value ]] && found=1
-  done
-
-  [[ $found -eq 0 ]] && return 0
-
-  echo "'$value' is a value in (${(@kv)hash})"
-  exit 1
-}
-
-###
+    echo "'$value' is not equal to '$comparison'"
+    exit 1
+} # ]]]
+# FUNCTION: _zunit_assert_exists [[[
 # Assert the a path exists
-###
 function _zunit_assert_exists() {
-  local pathname=$1 filepath
+    local pathname=$1 filepath
 
-  # If filepath is relative, prepend the test directory
-  if [[ "${pathname:0:1}" != "/" ]]; then
-    filepath="$testdir/${pathname}"
-  else
-    filepath="$pathname"
-  fi
+    # If filepath is relative, prepend the test directory
+    if [[ "${pathname:0:1}" != "/" ]]; then
+        filepath="$testdir/${pathname}"
+    else
+        filepath="$pathname"
+    fi
 
-  [[ -e "$filepath" ]] && return 0
+    [[ -e "$filepath" ]] && return 0
 
-  echo "'$pathname' does not exist"
-  exit 1
-}
+    echo "'$pathname' does not exist"
+    exit 1
+} # ]]]
+# FUNCTION: _zunit_assert_in [[[
+# Assert that a value is found in an array
+function _zunit_assert_in() {
+    local i found=0 value=$1
+    local -a array
+    array=(${(@)@:2})
 
-###
-# Assert the a path exists and is a file
-###
-function _zunit_assert_is_file() {
-  local pathname=$1 filepath
+    for i in ${(@f)array}; do
+        [[ $i = $value ]] && found=1
+    done
 
-  # If filepath is relative, prepend the test directory
-  if [[ "${pathname:0:1}" != "/" ]]; then
-    filepath="$testdir/${pathname}"
-  else
-    filepath="$pathname"
-  fi
 
-  [[ -f "$filepath" ]] && return 0
+    [[ $found -eq 1 ]] && return 0
 
-  echo "'$pathname' does not exist or is not a file"
-  exit 1
-}
-
-###
+    echo "'$value' is not in (${(@f)array})"
+    exit 1
+} # ]]]
+# FUNCTION: _zunit_assert_is_dir [[[
 # Assert the a path exists and is a directory
-###
 function _zunit_assert_is_dir() {
-  local pathname=$1 filepath
+    local pathname=$1 filepath
 
-  # If filepath is relative, prepend the test directory
-  if [[ "${pathname:0:1}" != "/" ]]; then
-    filepath="$testdir/$pathname"
-  else
-    filepath="$pathname"
-  fi
+    # If filepath is relative, prepend the test directory
+    if [[ "${pathname:0:1}" != "/" ]]; then
+        filepath="$testdir/$pathname"
+    else
+        filepath="$pathname"
+    fi
 
-  [[ -d "$filepath" ]] && return 0
+    [[ -d "$filepath" ]] && return 0
 
-  echo "'$pathname' does not exist or is not a directory"
-  exit 1
-}
+    echo "'$pathname' does not exist or is not a directory"
+    exit 1
+} # ]]]
+# FUNCTION: _zunit_assert_is_empty [[[
+# Assert that a value is empty
+function _zunit_assert_is_empty() {
+    local value=$1
 
-###
-# Assert the a path exists and is a symbolic link
-###
-function _zunit_assert_is_link() {
-  local pathname=$1 filepath
+    [[ -z ${value[@]} ]] && return 0
 
-  # If filepath is relative, prepend the test directory
-  if [[ "${pathname:0:1}" != "/" ]]; then
-    filepath="$testdir/${pathname}"
-  else
-    filepath="$pathname"
-  fi
-
-  [[ -h "$filepath" ]] && return 0
-
-  echo "'$pathname' does not exist or is not a symbolic link"
-  exit 1
-}
-
-###
-# Assert the a path exists and is readable
-###
-function _zunit_assert_is_readable() {
-  local pathname=$1 filepath
-
-  # If filepath is relative, prepend the test directory
-  if [[ "${pathname:0:1}" != "/" ]]; then
-    filepath="$testdir/${pathname}"
-  else
-    filepath="$pathname"
-  fi
-
-  [[ -r "$filepath" ]] && return 0
-
-  echo "'$pathname' does not exist or is not readable"
-  exit 1
-}
-
-###
-# Assert the a path exists and is writable
-###
-function _zunit_assert_is_writable() {
-  local pathname=$1 filepath
-
-  # If filepath is relative, prepend the test directory
-  if [[ "${pathname:0:1}" != "/" ]]; then
-    filepath="$testdir/${pathname}"
-  else
-    filepath="$pathname"
-  fi
-
-  [[ -w "$filepath" ]] && return 0
-
-  echo "'$pathname' does not exist or is not writable"
-  exit 1
-}
-
-###
+    echo "'${value[@]}' is not empty"
+    exit 1
+} # ]]]
+# FUNCTION: _zunit_assert_is_executable [[[
 # Assert the a path exists and is executable
-###
 function _zunit_assert_is_executable() {
-  local pathname=$1 filepath
+    local pathname=$1 filepath
 
-  # If filepath is relative, prepend the test directory
-  if [[ "${pathname:0:1}" != "/" ]]; then
-    filepath="$testdir/${pathname}"
-  else
-    filepath="$pathname"
-  fi
+    # If filepath is relative, prepend the test directory
+    if [[ "${pathname:0:1}" != "/" ]]; then
+        filepath="$testdir/${pathname}"
+    else
+        filepath="$pathname"
+    fi
 
-  [[ -x "$filepath" ]] && return 0
+    [[ -x "$filepath" ]] && return 0
 
-  echo "'$pathname' does not exist or is not executable"
-  exit 1
-}
+    echo "'$pathname' does not exist or is not executable"
+    exit 1
+} # ]]]
+# FUNCTION: _zunit_assert_is_file [[[
+# Assert the a path exists and is a file
+function _zunit_assert_is_file() {
+    local pathname=$1 filepath
+
+    # If filepath is relative, prepend the test directory
+    if [[ "${pathname:0:1}" != "/" ]]; then
+        filepath="$testdir/${pathname}"
+    else
+        filepath="$pathname"
+    fi
+
+    [[ -f "$filepath" ]] && return 0
+
+    echo "'$pathname' does not exist or is not a file"
+    exit 1
+} # ]]]
+# FUNCTION: _zunit_assert_is_greater_than [[[
+# Assert that an integer is greater than the comparison
+function _zunit_assert_is_greater_than() {
+    local value=$1 comparison=$2
+
+    [[ $value -gt $comparison ]] && return 0
+
+    echo "'$value' is not greater than '$comparison'"
+    exit 1
+} # ]]]
+# FUNCTION: _zunit_assert_is_key_in [[[
+# Assert that a value is a key in a hash
+function _zunit_assert_is_key_in() {
+    local i found=0 value=$1
+    local -A hash
+    hash=(${(@)@:2})
+
+    for k v in ${(@kv)hash}; do
+        [[ $k = $value ]] && found=1
+    done
+
+    [[ $found -eq 1 ]] && return 0
+
+    echo "'$value' is not a key in (${(@kv)hash})"
+    exit 1
+} # ]]]
+# FUNCTION: _zunit_assert_is_less_than [[[
+# Assert that an integer is less than the comparison
+function _zunit_assert_is_less_than() {
+    local value=$1 comparison=$2
+
+    [[ $value -lt $comparison ]] && return 0
+
+    echo "'$value' is not less than '$comparison'"
+    exit 1
+} # ]]]
+# FUNCTION: _zunit_assert_is_link [[[
+# Assert the a path exists and is a symbolic link
+function _zunit_assert_is_link() {
+    local pathname=$1 filepath
+
+    # If filepath is relative, prepend the test directory
+    if [[ "${pathname:0:1}" != "/" ]]; then
+        filepath="$testdir/${pathname}"
+    else
+        filepath="$pathname"
+    fi
+
+    [[ -h "$filepath" ]] && return 0
+
+    echo "'$pathname' does not exist or is not a symbolic link"
+    exit 1
+} # ]]]
+# FUNCTION: _zunit_assert_is_negative [[[
+# Assert that an integer is negative
+function _zunit_assert_is_negative() {
+    local value=$1 comparison=$2
+
+    [[ $value -lt 0 ]] && return 0
+
+    echo "'$value' is not negative"
+    exit 1
+} # ]]]
+# FUNCTION: _zunit_assert_is_not_empty [[[
+# Assert that a value is not empty
+function _zunit_assert_is_not_empty() {
+    local value=$1
+
+    [[ -n ${value[@]} ]] && return 0
+
+    echo "value is empty"
+    exit 1
+} # ]]]
+# FUNCTION: _zunit_assert_is_not_key_in [[[
+# Assert that a value is not a key in a hash
+function _zunit_assert_is_not_key_in() {
+    local i found=0 value=$1
+    local -A hash
+    hash=(${(@)@:2})
+
+    for k v in ${(@kv)hash}; do
+        [[ $k = $value ]] && found=1
+    done
+
+    [[ $found -eq 0 ]] && return 0
+
+    echo "'$value' is a key in (${(@kv)hash})"
+    exit 1
+} # ]]]
+# FUNCTION: _zunit_assert_is_not_substring_of [[[
+# Assert one string is not a substring of another
+function _zunit_assert_is_not_substring_of() {
+    local value=$1 comparison=$2
+
+    [[ "$comparison" != *"$value"* ]] && return 0
+
+    echo "'$value' is a substring of '$comparison'"
+    exit 1
+} # ]]]
+# FUNCTION: _zunit_assert_is_not_value_in [[[
+# Assert that a value is not a value in a hash
+function _zunit_assert_is_not_value_in() {
+    local i found=0 value=$1
+    local -A hash
+    hash=(${(@)@:2})
+
+    for k v in ${(@kv)hash}; do
+        [[ $v = $value ]] && found=1
+    done
+
+    [[ $found -eq 0 ]] && return 0
+
+    echo "'$value' is a value in (${(@kv)hash})"
+    exit 1
+} # ]]]
+# FUNCTION: _zunit_assert_is_positive [[[
+# Assert that an integer is positive
+function _zunit_assert_is_positive() {
+    local value=$1 comparison=$2
+
+    [[ $value -gt 0 ]] && return 0
+
+    echo "'$value' is not positive"
+    exit 1
+} # ]]]
+# FUNCTION: _zunit_assert_is_readable [[[
+# Assert the a path exists and is readable
+function _zunit_assert_is_readable() {
+    local pathname=$1 filepath
+
+    # If filepath is relative, prepend the test directory
+    if [[ "${pathname:0:1}" != "/" ]]; then
+        filepath="$testdir/${pathname}"
+    else
+        filepath="$pathname"
+    fi
+
+    [[ -r "$filepath" ]] && return 0
+
+    echo "'$pathname' does not exist or is not readable"
+    exit 1
+} # ]]]
+# FUNCTION: _zunit_assert_is_substring_of [[[
+# Assert one string is a substring of another
+function _zunit_assert_is_substring_of() {
+    local value=$1 comparison=$2
+
+    [[ "$comparison" = *"$value"* ]] && return 0
+
+    echo "'$value' is not a substring of '$comparison'"
+    exit 1
+} # ]]]
+# FUNCTION: _zunit_assert_is_value_in [[[
+# Assert that a value is a value in a hash
+function _zunit_assert_is_value_in() {
+    local i found=0 value=$1
+    local -A hash
+    hash=(${(@)@:2})
+
+    for k v in ${(@kv)hash}; do
+        [[ $v = $value ]] && found=1
+    done
+
+    [[ $found -eq 1 ]] && return 0
+
+    echo "'$value' is not a value in (${(@kv)hash})"
+    exit 1
+} # ]]]
+# FUNCTION: _zunit_assert_is_writable [[[
+# Assert the a path exists and is writable
+function _zunit_assert_is_writable() {
+    local pathname=$1 filepath
+
+    # If filepath is relative, prepend the test directory
+    if [[ "${pathname:0:1}" != "/" ]]; then
+        filepath="$testdir/${pathname}"
+    else
+        filepath="$pathname"
+    fi
+
+    [[ -w "$filepath" ]] && return 0
+
+    echo "'$pathname' does not exist or is not writable"
+    exit 1
+} # ]]]
+# FUNCTION: _zunit_assert_matches [[[
+# Assert that the value matches a regex pattern
+function _zunit_assert_matches() {
+    local value=$1 pattern=$2
+
+    [[ $value =~ $pattern ]] && return 0
+
+    echo "'$value' does not match /$pattern/"
+    exit 1
+} # ]]]
+# FUNCTION: _zunit_assert_not_equal_to [[[
+# Assert that two integers are not equal
+function _zunit_assert_not_equal_to() {
+    local value=$1 comparison=$2
+
+    [[ $value -ne $comparison ]] && return 0
+
+    echo "'$value' is equal to '$comparison'"
+    exit 1
+} # ]]]
+# FUNCTION: _zunit_assert_not_in [[[
+# Assert that a value is not found in an array
+function _zunit_assert_not_in() {
+    local i found=0 value=$1
+    local -a array
+    array=(${(@)@:2})
+
+    for i in ${(@f)array}; do
+        [[ $i = $value ]] && found=1
+    done
+
+    [[ $found -eq 0 ]] && return 0
+
+    echo "'$value' is in (${(@f)array})"
+    exit 1
+} # ]]]
+# FUNCTION: _zunit_assert_same_as [[[
+# Assert that two string are the same
+function _zunit_assert_same_as() {
+    local value=$1 comparison=$2
+
+    [[ $value = $comparison ]] && return 0
+
+    echo "'$value' is not the same as '$comparison'"
+    exit 1
+} # ]]]
--- src/zunit.zsh
+++ src/zunit.zsh
@@ -6,98 +6,95 @@ setopt extendedglob typesetsilent
 # Main zunit process #
 ######################
 
-###
+# FUNCTION: _zunit_usage [[[
 # Output usage information and exit
-###
 function _zunit_usage() {
-  echo "$(color yellow 'Usage:')"
-  echo "  zunit [options] [command] [tests...]"
-  echo
-  echo "$(color yellow 'Commands:')"
-  echo "  init               Bootstrap zunit in a new project"
-  echo "  run [tests...]     Run tests"
-  echo
-  echo "$(color yellow 'Options:')"
-  echo "  -h, --help         Output help text and exit"
-  echo "  -f, --fail-fast    Stop the test runner immediately after the first failure"
-  echo "  -r, --revolver     Run tests with revolver spinner"
-  echo "  -t, --tap          Output results in a TAP compatible format"
-  echo "  -v, --version      Output version information and exit"
-  echo "      --allow-risky  Supress warnings generated for risky tests"
-  echo "      --output-html  Print results to a HTML page"
-  echo "      --output-text  Print results to a text log, in TAP compatible format"
-  echo "      --time-limit   Set a time limit in seconds for each test"
-  echo "      --verbose      Prints full output from each test"
-}
-
-###
+    echo "$(color yellow 'Usage:')"
+    echo "  zunit [options] [command] [tests...]"
+    echo
+    echo "$(color yellow 'Commands:')"
+    echo "  init               Bootstrap zunit in a new project"
+    echo "  run [tests...]     Run tests"
+    echo
+    echo "$(color yellow 'Options:')"
+    echo "  -h, --help         Output help text and exit"
+    echo "  -f, --fail-fast    Stop the test runner immediately after the first failure"
+    echo "  -r, --revolver     Run tests with revolver spinner"
+    echo "  -t, --tap          Output results in a TAP compatible format"
+    echo "  -v, --version      Output version information and exit"
+    echo "      --allow-risky  Supress warnings generated for risky tests"
+    echo "      --output-html  Print results to a HTML page"
+    echo "      --output-text  Print results to a text log, in TAP compatible format"
+    echo "      --time-limit   Set a time limit in seconds for each test"
+    echo "      --verbose      Prints full output from each test"
+} # ]]]
+# FUNCTION: _zunit_version [[[
 # Output the version number
-###
 function _zunit_version() {
-  echo '0.8.2'
-}
-
-###
+    echo '0.9.0'
+} # ]]]
+# FUNCTION: _zunit [[[
 # The main zunit process
-###
 function _zunit() {
-  local help version ctx="$1" missing_dependencies=0 missing_config=1
-  if [[ -f .zunit.yml ]]; then
-    # Try and parse the config file within a subprocess,
-    # to avoid killing the main thread
-    $(eval $(_zunit_parse_yaml .zunit.yml 'zunit_config_') >/dev/null 2>&1)
-    if [[ $? -eq 0 ]]; then
-      # The config file was parsed successfully, so we Perform the parse
-      # again, but this time on the main thread so that the config vars are
-      # loaded into the enviroment
-      eval $(_zunit_parse_yaml .zunit.yml 'zunit_config_') >/dev/null 2>&1
-      missing_config=0
-    else
-      # The config file failed to parse, so we report this to the user and exit
-      print -P "%F{red}[ERROR]%f: %F{white}Unable to parse configuration file%f" >&2
-      exit 1
+    local help version ctx="$1" missing_dependencies=0 missing_config=1
+    if [[ -f .zunit.yml ]]; then
+        # Try and parse the config file within a subprocess,
+        # to avoid killing the main thread
+        $(eval $(_zunit_parse_yaml .zunit.yml 'zunit_config_') >/dev/null 2>&1)
+        if [[ $? -eq 0 ]]; then
+            # The config file was parsed successfully, so we Perform the parse
+            # again, but this time on the main thread so that the config vars are
+            # loaded into the enviroment
+            eval $(_zunit_parse_yaml .zunit.yml 'zunit_config_') >/dev/null 2>&1
+            missing_config=0
+        else
+            # The config file failed to parse, so we report this to the user and exit
+            print -P "%F{red}[ERROR]%f: %F{white}Unable to parse configuration file%f" >&2
+            exit 1
+        fi
     fi
-  fi
 
-  zparseopts -D -E \
-    h=help -help=help \
-    v=version -version=version
+    zparseopts -D -E \
+        h=help -help=help \
+        v=version -version=version
 
-  # If the version option is passed,
-  # output version information and exit
-  if [[ -n $version ]]; then
-    _zunit_version && exit 0
-  fi
+    # If the version option is passed,
+    # output version information and exit
+    if [[ -n $version ]]; then
+        _zunit_version && exit 0
+    fi
 
-  # Check which command has been passed, and run it. If the command
-  # is not recognised, then we'll assume it's a test file and pass
-  # it to `zunit run`, since that will catch it if it's not a valid file
-  case "$ctx" in
-    init )
-      # If the help option is passed,
-      # output usage information and exit
-      if [[ -n $help ]]; then
-        _zunit_init_usage && exit 0
-      fi
-      _zunit_init "${(@)@:2}"
-      ;;
-    run )
-      # If the help option is passed,
-      # output usage information and exit
-      if [[ -n $help ]]; then
-        _zunit_run_usage && exit 0
-      fi
-      _zunit_run "${(@)@:2}"
-      ;;
-    * )
-      # If the help option is passed,
-      # output usage information and exit
-      if [[ -n $help ]]; then
-        _zunit_usage && exit 0
-      fi
-      _zunit_run "$@"
-      ;;
-  esac
-}
+    # Check which command has been passed, and run it. If the command
+    # is not recognised, then we'll assume it's a test file and pass
+    # it to `zunit run`, since that will catch it if it's not a valid file
+    case "$ctx" in
+        init )
+            # If the help option is passed,
+            # output usage information and exit
+            if [[ -n $help ]]; then
+                _zunit_init_usage && exit 0
+            fi
+            _zunit_init "${(@)@:2}"
+            ;;
+        run )
+            # If the help option is passed,
+            # output usage information and exit
+            if [[ -n $help ]]; then
+                _zunit_run_usage && exit 0
+            fi
+            _zunit_run "${(@)@:2}"
+            ;;
+        * )
+            # If the help option is passed,
+            # output usage information and exit
+            if [[ -n $help ]]; then
+                _zunit_usage && exit 0
+            fi
+            _zunit_run "$@"
+            ;;
+    esac
+} # ]]]
 
 _zunit "$@"
+
+# vim: ft=zsh sw=4 ts=4 et foldmarker=[[[,]]] foldmethod=marker
